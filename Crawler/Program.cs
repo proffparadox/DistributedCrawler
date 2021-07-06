f7fc42f23296c939a89f3d165a0dfedb278bc8f8
Michael <michaelbarry564@gmail.com>
2021-07-06 23:03:50 +0100

    Updates Urls to be a serializer class for json parsing. Added Token class for json serialisation and de-serialisation

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -3,39 +3,54 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace NetCore.Docker {
     public class Program {
-        static HttpClient client = new HttpClient();
-        public static async Task<string> getToken() {
-            string token = "";
-            HttpResponseMessage response = await client.GetAsync("api/register");
+        static HttpClient _client = new HttpClient();
+
+        private static async Task<Token> GetToken() {
+            string stoken = "";
+            HttpResponseMessage response = await _client.GetAsync("api/register");
             if (response.IsSuccessStatusCode) {
-                token = await response.Content.ReadAsStringAsync();
+                stoken = await response.Content.ReadAsStringAsync();
             }
+
+            Token token = JsonSerializer.Deserialize<Token>(stoken);
             return token;
         }
 
-        public static async Task<List<Uri>> GetUrisAsync(string token) {
-            var uris = new List<Uri>();
+        public static async Task<List<Urls>> GetUrisAsync(Token token) {
+            var uris = new List<Urls>();
             string json = "";
-            HttpResponseMessage response = await client.GetAsync($"api/getJobs&token={token}");
+            HttpResponseMessage response = await _client.GetAsync($"api/getJobs&token={token.token}");
             if (response.IsSuccessStatusCode) {
                  json = await response.Content.ReadAsStringAsync();
             }
+            uris = JsonSerializer.Deserialize<List<Urls>>(json);
+            return uris;
         }
 
         public static void Main(string[] args) {
             RunAsync().GetAwaiter().GetResult();
         }
 
-        public static async Task RunAsync() {
-            client.BaseAddress = new Uri("www.laserbeam897.co.uk");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("appplication/json"));
-            string token = await getToken();
-            
+        private static async Task RunAsync() {
+            //_client.BaseAddress = new Uri("https://www.laserbeam897.co.uk");
+            //_client.DefaultRequestHeaders.Accept.Clear();
+            //_client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("appplication/json"));
+            //string token = await GetToken();
+
+            var testList = new List<string>();
+            testList.Add("www.google.com");
+            testList.Add("www.amazon.co.uk");
+            var urls = new Urls()
+            {
+                urls = testList
+            };
+            string json = JsonSerializer.Serialize(urls);
+            Console.WriteLine(json);
         }
      
     }
